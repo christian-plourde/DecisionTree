0bfdf4f5ce3dfea44beb0df24cc489b0486559b9
christian-plourde <christian.bertin.plourde@gmail.com>
2020-02-24 09:29:19 -0500

    first commit

---
+++ DecisionTree/DecisionTree.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionTree
+{
+    public abstract class DecisionTreeNode
+    {
+        DecisionTreeNode parent;
+        DecisionTreeNode leftChild;
+        DecisionTreeNode rightChild;
+
+        public DecisionTreeNode Left
+        {
+            get { return leftChild; }
+            set { leftChild = value;
+                leftChild.parent = this;
+            }
+        }
+
+        public DecisionTreeNode Right
+        {
+            get { return rightChild; }
+            set { rightChild = value;
+                rightChild.parent = this;
+            }
+        }
+
+        public bool IsLeaf
+        {
+            get { return Left == null && Right == null; }
+        }
+
+        public DecisionTreeNode()
+        {
+            
+        }
+
+        public abstract bool Evaluate();
+    }
+
+    public class CheckNode : DecisionTreeNode
+    {
+        Func<bool> condition;
+
+        public CheckNode(Func<bool> param)
+        {
+            this.condition = param;
+        }
+
+        public override bool Evaluate()
+        {
+            return condition();
+        }
+    }
+
+    public class ActionNode : DecisionTreeNode
+    {
+        Action action;
+
+        public ActionNode(Action action)
+        {
+            this.action = action;
+        }
+
+        public override bool Evaluate()
+        {
+            try
+            {
+                action();
+            }
+
+            catch
+            {
+                return false;
+            }
+            
+            return true;
+        }
+    }
+
+    public class DecisionTree
+    {
+        DecisionTreeNode root;
+
+        public DecisionTree(DecisionTreeNode root)
+        {
+            this.root = root;
+        }
+
+        public bool Evaluate()
+        {
+            bool res = false;
+            Evaluate(root, ref res);
+            return res;
+        }
+
+        private void Evaluate(DecisionTreeNode n, ref bool res)
+        {
+            if (n.IsLeaf)
+            {
+                res = n.Evaluate();
+                return;
+            }
+
+            //if evaluation of node is true go right (right for success, left for failure)
+            if (n.Evaluate())
+                Evaluate(n.Right, ref res);
+
+            else
+                Evaluate(n.Left, ref res);
+        }
+    }
+}
---
+++ DecisionTree/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionTree
+{
+    class Character
+    {
+        public bool tired;
+        public bool hungry;
+
+        public Character()
+        {
+            
+        }
+
+        public bool IsTired()
+        {
+            return tired;
+        }
+
+        public bool IsHungry()
+        {
+            return hungry;
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("Eating");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("Sleeping");
+        }
+
+        public void EatAndSleep()
+        {
+            Eat();
+            Sleep();
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Character character = new Character();
+            character.tired = true;
+            character.hungry = true;
+            CheckNode node = new CheckNode(character.IsTired);
+            CheckNode n2 = new CheckNode(character.IsHungry);
+            CheckNode n3 = new CheckNode(character.IsHungry);
+            ActionNode n4 = new ActionNode(character.Sleep);
+            ActionNode n5 = new ActionNode(character.Eat);
+            ActionNode n6 = new ActionNode(character.Sleep);
+            ActionNode n7 = new ActionNode(character.EatAndSleep);
+
+            node.Left = n2;
+            node.Right = n3;
+
+            n2.Left = n4;
+            n2.Right = n5;
+            n3.Left = n6;
+            n3.Right = n7;
+
+            DecisionTree tree = new DecisionTree(node);
+            tree.Evaluate();
+            Console.ReadLine();
+
+        }
+    }
+}
